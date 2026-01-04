8fdccbd50e167e912bb19e6e71f3ed8b25dbff7a
Eliaza Mae Malibiran <eliazamaemalibiran@gmail.com>
2026-01-04 20:15:48 +0800

    removed redundant bakery shop folder

--- bakery shop/includes/login-footer.php
+++
@@ -1,9 +0,0 @@
-    </div>
-  </div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</script>
-</body>
-</html>
